29c1eac25d3ad466d4dc4633b158bd7875fe8c96
yui0308ks <kazuyu03081@gmail.com>
2020-03-17 15:45:10 +0900

    パネルをページに追加

--- js/main.js
+++ js/main.js
@@ -6,14 +6,26 @@
       this.el = document.createElement('li');
       this.el.classList.add('pressed');
     }
+
+    getEl() {
+      return this.el;
+    }
   }
-  
+
   class Board {
     constructor() {
       this.panels = [];
       for (let i = 0; i < 4; i ++) {
-        this.panels.push(new this.panels());
+        this.panels.push(new Panel());
       }
+      this.setup();
+    }
+
+    setup() {
+      const board = document.getElementById('board');
+      this.panels.forEach(panel => {
+        board.appendChild(panel.getEl());
+      });
     }
   }
 
